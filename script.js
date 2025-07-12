a4a0650f1e69ba39f397c8685fbb4047307cb834
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-12 05:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
-  //your JS code here.
+  // Edge case: Empty string
+  if (!str) return false;
+
+  // Regular expression for validating email
+  const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
+
+  return emailRegex.test(str);
 }
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
